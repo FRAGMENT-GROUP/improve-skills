90432754fead7d0a21e3f38c6593de5f88fcb513
Andrey Komarovsky <komarovsky.andrey.prog@gmail.com>
2021-07-18 17:18:53 +0300

    fix: change length of line

--- grasshopper-messi-goals-function/grasshopper_messi_goals_function.js
+++ grasshopper-messi-goals-function/grasshopper_messi_goals_function.js
@@ -3,9 +3,10 @@
  * @link https://www.codewars.com/kata/55f73be6e12baaa5900000d4
  * @level 8kyu
  * @author Andrey Komarovsky
- * @param laLigaGoals Amount of goals in LaLiga league (Number)
- * @param copaDelReyGoals Amount of goals in Copa del Rey league (Number)
- * @param championsLeagueGoals Amount of goals in Champions league (Number)
+ * @param laGoals Amount of goals in LaLiga league (Number)
+ * @param copaGoals Amount of goals in Copa del Rey league (Number)
+ * @param championsGoals Amount of goals in Champions league (Number)
 */
 
-module.exports.goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+module.exports.goals = (laGoals, copaGoals, championsGoals) => laGoals  
++ copaGoals + championsGoals;
