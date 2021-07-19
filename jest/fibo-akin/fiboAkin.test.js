015c022d22a125a261636bd275d4afcfb6c68954
Your Name <sashakorol584@mail.ru>
2021-07-20 02:48:22 +0300

    task/cw-0050 new jest test

--- jest/fibo-akin/fiboAkin.test.js
+++ jest/fibo-akin/fiboAkin.test.js
@@ -1,5 +1,6 @@
 const lengthSupUK = require('../../tasks/fibo-akin/fiboAkin');
 const comp = require('../../tasks/fibo-akin/fiboAkin');
+
 describe('Simple tests for task:Fibo akin, function: lengthSupUk', () => {
   test('should return 6', () => {
     expect(lengthSupUK(50, 25)).toBe(6);
@@ -14,6 +15,7 @@ describe('Simple tests for task:Fibo akin, function: lengthSupUk', () => {
     expect(lengthSupUK(3177, 573)).not.toBe(2048);
   });
 });
+
 describe('Simple tests for task:Fibo akin, function: comp', () => {
   test('should return 37128', () => {
     expect(comp(74626)).toBe(37128);
