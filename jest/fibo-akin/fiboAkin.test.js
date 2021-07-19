4b9c90bb8a197e20582a13f04bd1b6242e6a5f18
Your Name <sashakorol584@mail.ru>
2021-07-20 02:28:45 +0300

    task/cw-0050 fix jest test

--- jest/fibo-akin/fiboAkin.test.js
+++ jest/fibo-akin/fiboAkin.test.js
@@ -1,43 +1,30 @@
 const lengthSupUK = require('../../tasks/fibo-akin/fiboAkin');
 const comp = require('../../tasks/fibo-akin/fiboAkin');
-
-
 describe('Simple tests for task:Fibo akin, function: lengthSupUk', () => {
-
-    test('should return 6', () => {
-        expect(lengthSupUK(50, 25)).toBe(6)
-    })
-
-    test('should return 1559', () => {
-        expect(lengthSupUK(3332, 973)).toBe(1559)
-    })
-
-    test('shouldn`t return 1247', () => {
-        expect(lengthSupUK(3332, 973)).not.toBe(1247)
-    })
-
-    test('shouldn`t return 2048', () => {
-        expect(lengthSupUK(3177, 573)).not.toBe(2048)
-    })
-
-})
-
+  test('should return 6', () => {
+    expect(lengthSupUK(50, 25)).toBe(6);
+  });
+  test('should return 1559', () => {
+    expect(lengthSupUK(3332, 973)).toBe(1559);
+  });
+  test('shouldn`t return 1247', () => {
+    expect(lengthSupUK(3332, 973)).not.toBe(1247);
+  });
+  test('shouldn`t return 2048', () => {
+    expect(lengthSupUK(3177, 573)).not.toBe(2048);
+  });
+});
 describe('Simple tests for task:Fibo akin, function: comp', () => {
-
-    test('should return 37128', () => {
-        expect(comp(74626)).toBe(37128)
-    })
-
-    test('should return 35692', () => {
-        expect(comp(71749)).toBe(35692)
-    })
-
-    test('shouldn`t return 28282', () => {
-        expect(comp(56890)).not.toBe(28282)
-    })
-
-    test('shouldn`t return 30055', () => {
-        expect(comp(60441)).not.toBe(30055)
-    })
-
-})
+  test('should return 37128', () => {
+    expect(comp(74626)).toBe(37128);
+  });
+  test('should return 35692', () => {
+    expect(comp(71749)).toBe(35692);
+  });
+  test('shouldn`t return 28282', () => {
+    expect(comp(56890)).not.toBe(28282);
+  });
+  test('shouldn`t return 30055', () => {
+    expect(comp(60441)).not.toBe(30055);
+  });
+});
