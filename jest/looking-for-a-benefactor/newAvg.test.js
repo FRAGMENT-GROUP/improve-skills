42b16a90e6c7d0ea7b538dac29ac44b2ed8db2b2
Your Name <sashakorol584@mail.ru>
2021-07-19 22:27:55 +0300

    task/cw-0044 jest test

---
+++ jest/looking-for-a-benefactor/newAvg.test.js
@@ -0,0 +1,29 @@
+const { test, expect } = require('@jest/globals');
+const { error } = require('console');
+const newAvg = require('./newAvg');
+
+describe('Simple tests for task:looking for a benefactor', () => {
+
+    test('should return 628', () => {
+        expect(newAvg([14, 30, 5, 7, 9, 11, 16], 90)).toBe(628)
+    })
+
+    test('should return 645', () => {
+        expect(newAvg([14, 30, 5, 7, 9, 11, 15], 92)).toBe(645)
+    })
+
+    test('should return error', () => {
+        const anyErrorMessage = ''
+        function negativeValue() {
+            if(newAvg([1, 2, 3, 4, 5, 6, 7, 8], 3) <= 0)
+                throw anyErrorMessage
+        }
+        
+        expect(negativeValue).toThrowError(anyErrorMessage)
+    })
+
+    test('should be rounded up to the next integer', () => {
+        expect(newAvg([1, 2, 3, 4, 5, 6, 7, 8.4], 5)).not.toBe(8.600000000000001)
+    })
+
+})
