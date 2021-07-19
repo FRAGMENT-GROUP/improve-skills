a79df6b94eda882b357554f0fa93bac60f2f3424
Your Name <sashakorol584@mail.ru>
2021-07-19 23:13:07 +0300

    task/cw-0044 fix solution

--- tasks/looking-for-a-benefactor/newAvg.js
+++ tasks/looking-for-a-benefactor/newAvg.js
@@ -1,8 +1,7 @@
-function newAvg(arr, newavg){
-    const result = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0)
-    if(result <= 0) 
-      throw 'errror'
-    return Math.ceil(result)
+function newAvg(arr, newavg) {
+  const result = newavg * (arr.length + 1) - arr.reduce((a, b) => a + b, 0);
+  if (result <= 0) throw new Error('error');
+  return Math.ceil(result);
 }
 
-module.exports = newAvg
+module.exports = newAvg;
