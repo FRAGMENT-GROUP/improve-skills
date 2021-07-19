ea2afcc7adf47d6fa06c6eeaa6554762831d4431
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-19 22:11:32 +0300

    feat: add solution for the task do you speak english

---
+++ jest/0002-CW-do-you-speak-english/Do-you-speak-English.test.js
@@ -0,0 +1,8 @@
+const en = require('../../tasks/0002-CW-do-you-speak-english/Do-you-speak-English');
+
+describe('simple tests',()=>{
+    it('should return true',()=>{expect(en("englishassdsa")).toBe(true)});
+    it('should return true',()=>{expect(en("LiaEnGLiShAA")).toBe(true)});
+    it('should return false',()=>{expect(en("engiolish")).toBe(false)});
+    it('should return false',()=>{expect(en("London")).toBe(false)});
+  });
