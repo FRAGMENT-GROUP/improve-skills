b33a5fe9d5f1ad78c83c030e1472dec3ea2a66d1
Dimkaze <dmitry.voroniuk@gmail.com>
2021-07-20 20:44:50 +0300

    task-0053 test mocha

---
+++ test/StarCode/task-0053.mocha.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { sumPPG } = require('../../tasks/StarCode/sumPPGagain');
+
+describe('Simple tests for task:All Star Code Challenge #1, function: sumPPG', () => {
+  it('should return 75', () => {
+    assert.strictEqual(sumPPG({ ppg: 50 }, { ppg: 25 }), 75);
+  });
+  it('should return 4', () => {
+    assert.strictEqual(sumPPG({ ppg: 2 }, { ppg: 2 }), 4);
+  });
+  it('shouldn`t return 25', () => {
+    assert.notStrictEqual(sumPPG({ ppg: 12 }, { ppg: 12 }), 25);
+  });
+  it('shouldn`t return 5', () => {
+    assert.notStrictEqual(sumPPG(({ ppg: 12 }, { ppg: 1 }), 5));
+  });
+});
