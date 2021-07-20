10466ef9b2b7a8f1de6e4740d103a7161c192b4c
Your Name <sashakorol584@mail.ru>
2021-07-20 15:57:58 +0300

    task/cw-0052 solution

---
+++ tasks/phone-directory/phone-directory.js
@@ -0,0 +1,16 @@
+function phone(strng, num) {
+  const clean = strng.replace(/[^-0-9a-z\s+A-Z\n<>.']/g, " ");
+  const re = new RegExp("\\+*" + num);
+  const a = clean.split('\n').filter(x => re.test(x));
+  if (a.length > 1) 
+    return "Error => Too many people: " + num;
+  if (a.length === 0) 
+    return "Error => Not found: " + num;
+  const rre = new RegExp("\\+*" + num);
+  const c = a[0].replace(rre, "");
+  const name = c.match(/<.*>/)[0];
+  const ad = c.replace(/<.*>/, "").replace(/\s+/g, " ").trim();
+  return "Phone => " + num + ", Name => " + name.substring(1, name.length-1) + ", Address => " + ad;
+}
+
+module.exports = phone
