f172016525db4c24f00f51354f07c796802ee7e4
Your Name <sashakorol584@mail.ru>
2021-07-20 23:54:37 +0300

    task/cw-0058 fix jest test

--- jest/John-and-Ann-sign-up-for-Codewars/johnAndAnn.test.js
+++ jest/John-and-Ann-sign-up-for-Codewars/johnAndAnn.test.js
@@ -5,15 +5,15 @@ const sumAnn = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn
 
 describe('Simple tests for task: John and Ann sign up for Codewars', () => {
   test('should return 38', () => {
-    expect(john(11)).toBe(38);
+    expect(john(11)).toEqual(38);
   });
   test('should return 12', () => {
-    expect(ann(6)).toBe(12);
-  });
-  test('should return 4070', () => {
-    expect(sumJohn(115)).toBe(4070);
+    expect(ann(6)).toEqual(12);
   });
   test('should return 1729', () => {
-    expect(sumAnn(75)).toBe(1729);
+    expect(sumJohn(75)).toEqual(1729);
+  });
+  test('should return 1729', () => {
+    expect(sumAnn(150)).toEqual(6930);
   });
 });
