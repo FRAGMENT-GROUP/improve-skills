72a75f9907e9453550dcf924a61ad419cb9ffb91
zxc <70864999+WhatWasThatt@users.noreply.github.com>
2021-07-20 21:54:19 +0300

    Update phone-directory.js

--- tasks/phone-directory/phone-directory.js
+++ tasks/phone-directory/phone-directory.js
@@ -1,14 +1,14 @@
 function phone(strng, num) {
   const clean = strng.replace(/[^-0-9a-z\s+A-Z\n<>.']/g, ' ');
-  const re = new RegExp(`\\+*${num}`);
-  const a = clean.split('\n').filter((x) => re.test(x));
-  if (a.length > 1) { return `Error => Too many people: ${num}`; }
-  if (a.length === 0) { return `Error => Not found: ${num}`; }
-  const rre = new RegExp(`\\+*${num}`);
-  const c = a[0].replace(rre, '');
-  const name = c.match(/<.*>/)[0];
-  const ad = c.replace(/<.*>/, '').replace(/\s+/g, ' ').trim();
-  return `Phone => ${num}, Name => ${name.substring(1, name.length - 1)}, Address => ${ad}`;
+  const registry = new RegExp(`\\+*${num}`);
+  const people = clean.split('\n').filter((x) => registry.test(x));
+  if (people.length > 1) { return `Error => Too many people: ${num}`; }
+  if (people.length === 0) { return `Error => Not found: ${num}`; }
+  const registryRes = new RegExp(`\\+*${num}`);
+  const adress = people[0].replace(registryRes, '');
+  const name = adress.match(/<.*>/)[0];
+  const adressRes = adress.replace(/<.*>/, '').replace(/\s+/g, ' ').trim();
+  return `Phone => ${num}, Name => ${name.substring(1, name.length - 1)}, Address => ${adressRes}`;
 }
 
 module.exports = phone;
