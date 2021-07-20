27ab2bc4388120ce591f45ab43e7f9411624a256
DarkJediDJ <tumbus_druid@mail.ru>
2021-07-20 10:42:44 +0300

    Eslint fixed

--- tasks/0040-CW-Rainfall.js
+++ tasks/0040-CW-Rainfall.js
@@ -1,32 +1,31 @@
-funcion parseNumber (n) {
-  return n.replace(/[^0-9-.,]/g,'')
- }
- function mean(town, string) {
-string = string.split("\n")
-for (let i = 0; i <string.length;i++){
- if(string[i].substring(0,string[i].indexOf(":"))==town){
- string[i] = parseNumber(string[i])
-string[i]=string[i].split(",")
- return string[i].reduce(function(a, b) {return Number(a) + Number(b)})/12
+function parseNumber(n) {
+  return n.replace(/[^0-9-.,]/g, '');
 }
-
+function mean(town, string) {
+  string1 = string.split('\n');
+  for (let i = 0; i < string1.length; i += 1) {
+    if (string1[i].substring(0, string1[i].indexOf(':')) === town) {
+      string1[i] = parseNumber(string1[i]);
+      string1[i] = string1[i].split(',');
+      return string[i].reduce((a, b) => Number(a) + Number(b)) / 12;
+    }
+  }
+  return -1;
 }
-   return -1
+function variance(town, string) {
+  data = string;
+  string1 = String(string).split('\n');
+  for (let i = 0; i < string1.length; i += 1) {
+    if (string1[i].substring(0, string1[i].indexOf(':')) === town) {
+      string1[i] = parseNumber(string1[i]);
+      string1[i] = string1[i].split(',');
+      let sum = 0;
+      for (let j = 0; j < string1[i].length; j += 1) {
+        sum += (string[i][j] - mean(town, data)) ** 2;
+      }
+      return sum / 12;
+    }
+  }
+  return -1;
 }
- function variance(town, string) {
-     data = string
-  string =String( string).split("\n")
- for (let i = 0; i <string.length;i++){
- if(string[i].substring(0,string[i].indexOf(":"))==town){
- string[i] = parseNumber(string[i])
- string[i]=string[i].split(",")
-     let sum = 0
- for(let j = 0; j <string[i].length; j++){
-sum +=(string[i][j] - mean (town, data))**2
-}
-return sum/12
-}
-
- }   
-   return -1
-}t
+variance('London', 'Random_string');
