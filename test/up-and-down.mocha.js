c3ca6315d5cdaf4e81683c330eba3c027b23d3bf
forever-alone1 <danalexs27@gmail.com>
2021-07-20 21:07:35 +0300

    task/0073-CW fixed test mocha

--- test/up-and-down.mocha.js
+++ test/up-and-down.mocha.js
@@ -6,10 +6,10 @@ describe('simple tests', () => {
     assert.strictEqual(arrange(['who hit retaining The That a we taken']), 'who RETAINING hit THAT a THE we TAKEN');
   });
   it('Should return i CAME on WERE up GRANDMOTHERS so', () => {
-    assert.strictEqual(arrange(['on I came up were so grandmothers']),'i CAME on WERE up GRANDMOTHERS so');
+    assert.strictEqual(arrange(['on I came up were so grandmothers']), 'i CAME on WERE up GRANDMOTHERS so');
   });
   it('Should not return NaN', () => {
-    assert.notStrictEqual(arrange(['who hit retaining The That a we taken']),NaN);
+    assert.notStrictEqual(arrange(['who hit retaining The That a we taken']), NaN);
   });
   it('Should not return undefined', () => {
     assert.notStrictEqual(arrange(['on I came up were so grandmothers']), undefined);
