72a777f8ee94957603180232e6d2adc5be38d4ce
KarinaKlevzhits <72280071+KarinaKlevzhits@users.noreply.github.com>
2021-07-20 21:56:45 +0300

    task/0071 CW add fixed mocha test

--- test/moving-zeros-to-the-end/MovingZeros.mocha.js
+++ test/moving-zeros-to-the-end/MovingZeros.mocha.js
@@ -1,10 +1,10 @@
-const moveZeros = require('../../tasks/moving-zeros-to-the-end/MovingZeros');
 const assert = require('assert');
+const moveZeros = require('../../tasks/moving-zeros-to-the-end/MovingZeros');
 
 const arr1 = [1, 2, 0, 1, 0, 1, 0, 3, 0, 1];
 const resultArr1 = [1, 2, 1, 1, 3, 1, 0, 0, 0, 0];
 const arr2 = [false, 1, 1, 2, 1, 3, 'a', 0, 0];
-const resultArr2 = [false, 1, 1, 2, 1, 3,'a', 0, 0];
+const resultArr2 = [false, 1, 1, 2, 1, 3, 'a', 0, 0];
 
 describe('Simple test for task:Moving zeros to the end', () => {
   it('should be return [1,2,1,1,3,1,0,0,0,0]', () => {
