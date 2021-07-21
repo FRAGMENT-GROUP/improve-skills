67ec61de85d295ddd7f547bf705f2470595047dd
Dimkaze <dmitry.voroniuk@gmail.com>
2021-07-21 20:59:51 +0300

    task 0043 jest

--- test/StarCode/task-0045.mocha.js
+++ test/StarCode/task-0045.mocha.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 const { getDivisorsCnt } = require('../../tasks/StarCode/task-0045div');
 
-describe('Simple tests for task:All Star Code Challenge #1, function: factorial', () => {
-  it('should return 6', () => {
+describe('Simple tests for task:Count the divisors of a number, function: getDivisorsCnt', () => {
+  it('should return 2', () => {
     assert.strictEqual(getDivisorsCnt(3), 2);
   });
-  it('should return 120', () => {
+  it('should return 2', () => {
     assert.strictEqual(getDivisorsCnt(5), 2);
   });
   it('shouldn`t return 25', () => {
