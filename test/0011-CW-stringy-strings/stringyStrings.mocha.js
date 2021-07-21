c06537e050800c41ca163eea3adfcbc502bc632d
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 21:45:03 +0300

    refactor: changed the description, values and names of test cases for mocha tests

--- test/0011-CW-stringy-strings/stringyStrings.mocha.js
+++ test/0011-CW-stringy-strings/stringyStrings.mocha.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const { stringy } = require('../../tasks/0011-CW-stringy-strings/stringyStrings');
 
-describe('stringyString mocha', () => {
-  it('should return correct string', () => { assert.strictEqual(stringy(5), '10101'); });
-  it('should return correct string', () => { assert.strictEqual(stringy(1), '1'); });
-  it('should return correct string', () => { assert.strictEqual(stringy(-7), 'ValueError'); });
-  it('should return correct string', () => { assert.strictEqual(stringy(0), 'ValueError'); });
+describe('simple Mocha tests for task Stringy string', () => {
+  it('should return correct string - "10101010101010101010101010101010101010101010101010101010"', () => { assert.strictEqual(stringy(56), '10101010101010101010101010101010101010101010101010101010'); });
+  it('should return correct string - "1010101"', () => { assert.strictEqual(stringy(7), '1010101'); });
+  it('should return error message - "ValueError"', () => { assert.strictEqual(stringy(''), 'ValueError'); });
+  it('should return correct string - "ValueError"', () => { assert.strictEqual(stringy(-5), 'ValueError'); });
 });
