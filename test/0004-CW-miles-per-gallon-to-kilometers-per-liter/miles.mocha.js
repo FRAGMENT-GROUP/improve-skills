7a3d8cc88d4bb5c82d679c49c7705fb7a43ca786
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 20:53:53 +0300

    refactor: changed the description, values and names of test cases for jest tests

--- test/0004-CW-miles-per-gallon-to-kilometers-per-liter/miles.mocha.js
+++ test/0004-CW-miles-per-gallon-to-kilometers-per-liter/miles.mocha.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const { converter } = require('../../tasks/0004-CW-miles-per-gallon-to-kilometers-per-liter/miles');
 
-describe('converter Mocha', () => {
-  it('should return number', () => { assert.strictEqual(converter(10), 3.54); });
-  it('should return number', () => { assert.strictEqual(converter(20), 7.08); });
-  it('should return number', () => { assert.strictEqual(converter(0), 0); });
-  it('should return number', () => { assert.strictEqual(converter(30), 10.62); });
+describe('simple Mocha tests for task Miles per gallon to kilometers per liter', () => {
+  it('should return message "the number cant be negative"', () => { assert.strictEqual(converter(-10), 'the number cant be negative'); });
+  it('should return a number - 1416024176000000', () => { assert.strictEqual(converter(4000000000000000), 1416024176000000); });
+  it('should return a number - 7.43', () => { assert.strictEqual(converter(21), 7.43); });
+  it('should return "NaN" if the value is not a number', () => { assert.strictEqual(converter('aaa'), NaN); });
 });
