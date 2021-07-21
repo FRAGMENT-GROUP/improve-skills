9be59793f37985284c7bac015747d2ac1aba0c21
forever-alone1 <danalexs27@gmail.com>
2021-07-21 21:29:08 +0300

    task/0082-CW add solution

--- task/replace-alphabet.js
+++ task/replace-alphabet.js
@@ -1,13 +1,14 @@
 function alphabetPosition(text) {
-  const result = [];
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    text += '';
+    const result = [];
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
     'w', 'x', 'y', 'z'];
-  const textExample = text.replace(/\W*\d+/g, '').toLowerCase().split('');
-  for (let i = 0; i < textExample.length; i += 1) {
+    const textExample = text.replace(/\W*\d+/g, '').toLowerCase().split('');
+    for (let i = 0; i < textExample.length; i += 1) {
     const j = alphabet.indexOf(textExample[i]) + 1;
     if (j) result.push(j);
-  }
-  return result.join(' ');
+    }
+    return result.join(' ');
 }
 module.exports = alphabetPosition;
