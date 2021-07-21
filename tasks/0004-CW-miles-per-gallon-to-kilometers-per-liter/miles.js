a7afc427439a27c0fac7a2f8c92e3da637c714f8
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 20:45:42 +0300

    feat: added a check for a non-negative number

--- tasks/0004-CW-miles-per-gallon-to-kilometers-per-liter/miles.js
+++ tasks/0004-CW-miles-per-gallon-to-kilometers-per-liter/miles.js
@@ -1,1 +1,4 @@
-module.exports.converter = (mpg) => Math.round(mpg * 35.4006044) / 100;
+module.exports.converter = (mpg) => {
+  if (mpg <= 0) return 'the number cant be negative';
+  return Math.round(mpg * 35.4006044) / 100;
+};
