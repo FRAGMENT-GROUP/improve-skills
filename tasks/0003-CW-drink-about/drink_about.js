fd8cf4dc43724fbb78be86ac91bdf141f47621e5
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 21:20:47 +0300

    feat: added checks for value constraints and check for the absence of a value

--- tasks/0003-CW-drink-about/drink_about.js
+++ tasks/0003-CW-drink-about/drink_about.js
@@ -1,1 +1,7 @@
-module.exports.drinkAbout = (old) => (old < 14 ? 'drink toddy' : old < 18 ? 'drink coke' : old < 21 ? 'drink beer' : 'drink whisky');
+module.exports.drinkAbout = (old) => {
+  if (old === '' || old < 0 || old > 110) return 'Invalid value';
+  if (old >= 21) return 'drink whisky';
+  if (old >= 18) return 'drink beer';
+  if (old >= 14) return 'drink coke';
+  return 'drink toddy';
+};
