a90d357d6f9524bf010e23a07925f25a7f7fa31d
Your Name <sashakorol584@mail.ru>
2021-07-21 11:27:50 +0300

    task/cw-0058 fixed jest test

--- jest/John-and-Ann-sign-up-for-Codewars/johnAndAnn.test.js
+++ jest/John-and-Ann-sign-up-for-Codewars/johnAndAnn.test.js
@@ -1,17 +1,19 @@
-const john = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const ann = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const sumJohn = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const sumAnn = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
+const {
+  john,
+  ann,
+  sumJohn,
+  sumAnn,
+} = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
 
 describe('Simple tests for task: John and Ann sign up for Codewars', () => {
-  test('should return 38', () => {
-    expect(john(11)).toEqual(38);
+  test('should return [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]', () => {
+    expect(john(11)).toEqual([0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]);
   });
-  test('should return 12', () => {
-    expect(ann(6)).toEqual(12);
+  test('should return [1, 1, 2, 2, 3, 3]', () => {
+    expect(ann(6)).toEqual([1, 1, 2, 2, 3, 3]);
   });
   test('should return 1729', () => {
-    expect(sumJohn(75)).toEqual(1729);
+    expect(sumJohn(75)).toEqual(1720);
   });
   test('should return 1729', () => {
     expect(sumAnn(150)).toEqual(6930);
