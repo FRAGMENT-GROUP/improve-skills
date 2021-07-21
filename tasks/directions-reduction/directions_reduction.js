b90d4f8d75a69bea607c9533718d094471f61de7
FreisV <Ichvadikz@gmail.com>
2021-07-21 20:59:38 +0300

    task/0036 add task solution

---
+++ tasks/directions-reduction/directions_reduction.js
@@ -0,0 +1,33 @@
+module.exports.dirReduc = (arr) => {
+  for (let i = 0; i < 5; i += 1) {
+    switch (arr[i]) {
+      case 'NORTH':
+        if (arr[i + 1] === 'SOUTH') {
+          arr.splice(i, 2);
+          i = -1;
+        }
+        break;
+      case 'SOUTH':
+        if (arr[i + 1] === 'NORTH') {
+          arr.splice(i, 2);
+          i = -1;
+        }
+        break;
+      case 'EAST':
+        if (arr[i + 1] === 'WEST') {
+          arr.splice(i, 2);
+          i = -1;
+        }
+        break;
+      case 'WEST':
+        if (arr[i + 1] === 'EAST') {
+          arr.splice(i, 2);
+          i = -1;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  return arr;
+}
