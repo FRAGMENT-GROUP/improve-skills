911e2fbf85bd7e4a05a1ad06872247aeb80cd509
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 21:25:14 +0300

    refactor: changed the description, values and names of test cases for mocha tests

--- test/0003-CW-drink-about/drink-about.mocha.js
+++ test/0003-CW-drink-about/drink-about.mocha.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const { drinkAbout } = require('../../tasks/0003-CW-drink-about/drink_about');
 
-describe('drinkAbout Mocha', () => {
-  it('should return drink toddy', () => { assert.strictEqual(drinkAbout(2), 'drink toddy'); });
-  it('should return drink beer', () => { assert.strictEqual(drinkAbout(18), 'drink beer'); });
-  it('should return drink whisky', () => { assert.strictEqual(drinkAbout(67), 'drink whisky'); });
-  it('should return drink coke', () => { assert.strictEqual(drinkAbout(17), 'drink coke'); });
+describe('simple Mocha tests for task Drink about', () => {
+  it('should return message "drink toddy"', () => { assert.strictEqual(drinkAbout(2), 'drink toddy'); });
+  it('should return message "drink beer"', () => { assert.strictEqual(drinkAbout(18), 'drink beer'); });
+  it('should return error message "Invalid value"', () => { assert.strictEqual(drinkAbout(190), 'Invalid value'); });
+  it('should return error message "Invalid value"', () => { assert.strictEqual(drinkAbout(-15), 'Invalid value'); });
 });
