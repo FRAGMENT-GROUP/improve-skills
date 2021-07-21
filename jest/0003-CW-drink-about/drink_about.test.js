0de2267b45edda61a29a07a5b20ba8ab49742048
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-21 21:21:45 +0300

    refactor: changed the description, values and names of test cases for jest tests

--- jest/0003-CW-drink-about/drink_about.test.js
+++ jest/0003-CW-drink-about/drink_about.test.js
@@ -1,8 +1,8 @@
 const { drinkAbout } = require('../../tasks/0003-CW-drink-about/drink_about');
 
-describe('drinkAbout Jest', () => {
-  it('should return drink toddy', () => { expect(drinkAbout(2)).toBe('drink toddy'); });
-  it('should return drink beer', () => { expect(drinkAbout(18)).toBe('drink beer'); });
-  it('should return drink whisky', () => { expect(drinkAbout(67)).toBe('drink whisky'); });
-  it('should return drink coke', () => { expect(drinkAbout(17)).toBe('drink coke'); });
+describe('simple Jest tests for task Drink about', () => {
+  it('should return message "drink toddy"', () => { expect(drinkAbout(2)).toBe('drink toddy'); });
+  it('should return message "drink beer"', () => { expect(drinkAbout(18)).toBe('drink beer'); });
+  it('should return error message "Invalid value"', () => { expect(drinkAbout(-3)).toBe('Invalid value'); });
+  it('should return error message "Invalid value"', () => { expect(drinkAbout('')).toBe('Invalid value'); });
 });
