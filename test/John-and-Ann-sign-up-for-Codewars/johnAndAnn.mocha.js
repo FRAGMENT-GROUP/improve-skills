6320cff701e283628325406142a375c1d5c08b3e
Your Name <sashakorol584@mail.ru>
2021-07-21 11:27:19 +0300

    task/cw-0058 fixed mocha test

--- test/John-and-Ann-sign-up-for-Codewars/johnAndAnn.mocha.js
+++ test/John-and-Ann-sign-up-for-Codewars/johnAndAnn.mocha.js
@@ -1,18 +1,20 @@
 const assert = require('assert');
-const john = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const ann = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const sumJohn = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
-const sumAnn = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
+const {
+  john,
+  ann,
+  sumJohn,
+  sumAnn,
+} = require('../../tasks/John-and-Ann-sign-up-for-Codewars/johnAndAnn');
 
 describe('Simple tests for task: John and Ann sign up for Codewars', () => {
-  it('should return 38', () => {
-    assert.strictEqual(john(11), 38);
+  it('should return [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]', () => {
+    assert.deepStrictEqual(john(11), [0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6]);
   });
-  it('should return 12', () => {
-    assert.strictEqual(ann(6), 12);
+  it('should return [1, 1, 2, 2, 3, 3]', () => {
+    assert.deepStrictEqual(ann(6), [1, 1, 2, 2, 3, 3]);
   });
-  it('should return 1729', () => {
-    assert.strictEqual(sumJohn(75), 1729);
+  it('should return 1720', () => {
+    assert.strictEqual(sumJohn(75), 1720);
   });
   it('should return 6930', () => {
     assert.strictEqual(sumAnn(150), 6930);
