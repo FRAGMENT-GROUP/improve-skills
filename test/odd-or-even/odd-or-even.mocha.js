c51bf5016da074cf802bad05e3ed64cf4770fc47
Your name <pmodelist@bk.ru>
2021-07-23 12:33:19 +0300

    refactoring mistake on tests

--- test/odd-or-even/odd-or-even.mocha.js
+++ test/odd-or-even/odd-or-even.mocha.js
@@ -1,14 +1,11 @@
 const assert = require('assert');
-const {oddOrEven} = require('./taska1.js').oddOrEven;
-describe('#oddOreven()', ()=>{
-    describe('testing odd sum',()=>{
-        it('should not return even', () => {assert.notStrictEqual(oddOrEven([1,2]),'odd')});
-        it('should not return even', () => {assert.notStrictEqual(oddOrEven([1]),'odd')});
-        it('should not return even', () => {assert.strictEqual(oddOrEven([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]),'odd')});
-        it('should not return even', () => {assert.strictEqual(oddOrEven([1840357,2,2,4,1,2,1]),'odd')});
-    });
-    describe('testing even sum',()=>{
-        it('should return even', () => {assert.strictEqual(oddOrEven([1,2,3,4,5,6,7]),'even')});
-        it('should return even', () => {assert.strictEqual(oddOrEven([0]),'even')});
-    });
+const  oddOrEven  = require('../../tasks/odd-or-even/add_or_even');
+
+describe('#oddOrEven()', () => {
+    it('should return odd', () => { assert.strictEqual(oddOrEven([1, 2]), 'odd'); });
+    it('should return odd', () => { assert.strictEqual(oddOrEven([1]), 'odd'); });
+    it('should not return even', () => { assert.strictEqual(oddOrEven([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 'odd'); });
+    it('should not return even', () => { assert.strictEqual(oddOrEven([1840357, 2, 2, 4, 1, 2, 1]), 'odd'); });
+    it('should return even', () => { assert.strictEqual(oddOrEven([1, 2, 3, 4, 5, 6, 7]), 'even'); });
+    it('should return even', () => { assert.strictEqual(oddOrEven([0]), 'even'); });
 });
