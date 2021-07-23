f1a9fc95040d2b560698f060c89cb2411009c861
Your name <pmodelist@bk.ru>
2021-07-23 12:40:15 +0300

    refacroring mistakes with mocha test

--- tasks/odd-or-even/add_or_even.js
+++ tasks/odd-or-even/add_or_even.js
@@ -1,11 +1,11 @@
 function oddOrEven(arr) {
-    let sum = 0;
-    for(let i=0; i!=arr.length;++i){
-      sum+=arr[i];
-    }   
-    if(sum%2==0){
-      return 'even'
-    }
-    return 'odd'
+  let sum = 0;
+  for (let i = 0; i !== arr.length; i += 1) {
+    sum += arr[i];
   }
- module.export.oddOrEven = oddOrEven;
+  if (sum % 2 === 0) {
+    return 'even';
+  }
+  return 'odd';
+}
+module.exports = oddOrEven;
