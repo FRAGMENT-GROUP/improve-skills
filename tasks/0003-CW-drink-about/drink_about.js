e69fe8e1d2a6741d6baa229db215c9e8fd909eee
skripchenkoVeta <skripchenkoveta@gmail.com>
2021-07-23 17:54:41 +0300

    refactor: the method of solving the task has been changed

--- tasks/0003-CW-drink-about/drink_about.js
+++ tasks/0003-CW-drink-about/drink_about.js
@@ -1,7 +1,12 @@
 module.exports.drinkAbout = (old) => {
   if (old === '' || old < 0 || old > 110) return 'Invalid value';
-  if (old >= 21) return 'drink whisky';
-  if (old >= 18) return 'drink beer';
-  if (old >= 14) return 'drink coke';
-  return 'drink toddy';
+  switch (true) {
+    case (old >= 21):
+      return 'drink whisky';
+    case (old >= 18):
+      return 'drink beer';
+    case (old >= 14):
+      return 'drink coke';
+    default: return 'drink toddy';
+  }
 };
